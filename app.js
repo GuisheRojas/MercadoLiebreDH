bfb4df9d43aa1c1ecde4363fb80484ea7c790620
GuisheRojas <ifireflyisland@gmail.com>
2023-11-07 21:25:04 -0300

    Subida del proyecto MercadoLiebre

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express()
+const PORT = 8008;
+
+app.use(express.static(__dirname + '/'))
+
+app.get('/', function (req, res){
+    res.sendFile(__dirname + "/views/home.html")
+})
+app.get('/', function(req,res){
+    res.sendFile(__dirname + "/views/register.html")
+})
+app.listen(PORT, () =>{
+    console.log("Servidor iniciado en el port", PORT)
+})
